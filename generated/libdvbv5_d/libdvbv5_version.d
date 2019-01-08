65896083aa911ab6de720bd7e9a123630bee5a82
Russel Winder <russel@winder.org.uk>
2019-01-08 06:21:58 +0000

    Add a module statement to correct the module name.

---
+++ generated/libdvbv5_d/libdvbv5_version.d
@@ -0,0 +1,8 @@
+module libdvbv5_d.libdvbv5_version;
+
+extern (C):
+
+enum LIBDVBV5_VERSION_MAJOR = 1;
+enum LIBDVBV5_VERSION_MINOR = 14;
+enum LIBDVBV5_VERSION_PATCH = 2;
+enum LIBDVBV5_VERSION = "1.14.2";
